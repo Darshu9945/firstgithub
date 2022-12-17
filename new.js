b8b1ac2b5ed2213423f6540899665db6f68f994d
Darshu9945 <1ms18ch016@gmail.com>
2022-12-18 02:35:49 +0530

    added final project

--- new.js
+++ new.js
@@ -1,1 +1,28 @@
-console.log("hello world")
+var from = document.getElementById('addForm')
+var itemlist = document.getElementById('items')
+from.addEventListener('submit',additem)
+itemlist.addEventListener('click',removeitem)
+ function additem(e){
+    e.preventDefault();
+    var newitem = document.getElementById('item').value;
+    var li = document.createElement("li")
+    li.className='list-group-item'
+    var k1 = document.createTextNode(newitem)
+    li.appendChild(k1)
+    var deletebtn = document.createElement('button')
+    deletebtn.className='btn btn-danger btn-sm float-right delete'
+    deletebtn.appendChild(document.createTextNode('x'))
+    li.appendChild(deletebtn)
+    itemlist.appendChild(li)
+
+ }
+ function removeitem(e){
+    if (e.target.classList.contains('delete')){
+        if (confirm('are you sure')){
+            var li = e.target.parentElement
+            itemlist.removeChild(li)
+        }
+    }
+    
+
+ }
