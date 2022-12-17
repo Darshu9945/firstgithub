6b59e108adab64e6bf67327715c50b1e1400111d
Darshu9945 <1ms18ch016@gmail.com>
2022-12-17 23:24:14 +0530

    added some code

--- demo.js
+++ demo.js
@@ -1,2 +1,6 @@
-console.log("adharsh kanni")
-console.log('dfarshan k b')
+var k1 =document.getElementsByClassName("list-group-item");
+k1[2].style.backgroundColor='green'
+
+for (var i=0; i< k1.length ;i++){
+    k1[i].style.fontWeight='bold';
+}
