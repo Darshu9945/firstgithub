6deebf98b9989966450412dd7b5ae65960d6f81c
Darshu9945 <1ms18ch016@gmail.com>
2023-03-04 19:30:06 +0530

    added 1st api

--- gg1.js
+++ gg1.js
@@ -1,18 +1,49 @@
-var itemlist=document.getElementById('items');
-var addform = document.getElementById('addForm')
-addform.addEventListener('click',additem)
-itemlist.addEventListener('click',removeitem)
-function removeitem(e){
-    e.target.classList.contains('list-group')
-    var v1 = e.target.parentElement
-    console.log(v1)
-    // itemlist.removeChild(v1)
-    itemlist.removeChild(v1)
-}
-function additem(e){
-    e.target.classList.contains('form-control mr-2')
-    console.log(1)
-    var v1 = e.target.parentElement
-    console.log(v1)
-    addform.remove(v1)
-}
+const btn = document.getElementById('my-form')
+var nameinput=document.getElementById('kb3')
+var emailinput=document.getElementById('kb2')
+var msg =document.getElementById('kb5')
+var userlist=document.getElementById('mylist')
+btn.addEventListener('submit',adduser);
+
+ function adduser(e){
+    e.preventDefault();
+    if (nameinput.value==='' || emailinput.value===''){
+
+        msg.calasslist.add('error')
+
+        msg.innerHTML='please enter all the field';
+    }
+
+    else{
+        name=nameinput.value
+        email=emailinput.value
+        const obj={
+            name,
+            email
+        }
+        localStorage.setItem(emailinput.value,JSON.stringify(obj))
+        // localStorage.setItem('email',emailinput.value)
+        // let nameget=JSON.parse(localStorage.getItem('name'))
+        // console.log(nameget)
+        Object.keys(localStorage).forEach((key) => {
+                k1=localStorage.getItem(key);
+                detailsofpepole=JSON.parse(k1)
+                // console.log(Object.keys(detailsofpepole))
+                let b1 = Object.keys(detailsofpepole)
+                const li =document.createElement('li')
+                  b1.forEach(key => {
+                    var k3 =document.createTextNode(`${detailsofpepole[key]}  : `)
+                    li.appendChild(k3)
+                  console.log(detailsofpepole[key])
+
+            }
+
+                )
+
+                // console.log(detailsofpepole,'kb')
+
+                userlist.appendChild(li)
+        }
+        )
+    }
+ }
